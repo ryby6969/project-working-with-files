1446713eebd2c1d3dcd873da30c91fe72c9d35c0
Roy Davis <myroy1024@yahoo.com>
2016-03-29 09:05:41 -0400

    Working with files V.4

--- randomQuizGenerator.py
+++ randomQuizGenerator.py
@@ -28,8 +28,8 @@ capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
             'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
 #Generate 5 quiz files.
 for quizNum in range(5):
-    quizFile = open('./quizzes/capitalsquiz%s.txt' % (quizNum + 1),'w')
-    answerKeyFile = open('./answers/capitalsquiz_answers%s.txt' % (quizNum + 1),'w')
+    quizFile = open(os.path.join('./quizzes/capitalsquiz%s.txt' % (quizNum + 1)),'w')
+    answerKeyFile = open(os.path.join('./answers/capitalsquiz_answers%s.txt' % (quizNum + 1)),'w')
     
     quizFile.write('Name:\n\nDate:\n\nPeriod:\n\n')
     quizFile.write((' ' * 20) + 'State Capitals Quiz (Form %s)' % (quizNum + 1))
